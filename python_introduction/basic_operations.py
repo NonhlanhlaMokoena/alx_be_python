da50510b848484c01c7a1ebc789653c63454be3e
Nonhlanhla <mokoena3586@gmail.com>
2025-11-13 01:51:50 +0200

    Add basic_operations.py with basic arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Basic Arithmetic Exercise. Step 1: Declare a variable number and assign a value
+number1 = 10
+number2 = 5
+
+#Step 2: Arithmetic operations for addition, subtraction and multiplication
+addition = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Step 3: Printing the results of these operations in a human-readable format
+print("Addition", "of", number1, "and", number2, "is", addition)
+print("Subraction", "of", number1, "and", number2, "is", difference)
+print("Multiplication", "of", number1, "and", number2, "is", product)

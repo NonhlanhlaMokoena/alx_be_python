5b8044de09ca1426ac2495157af9ea808b0afe7c
Nonhlanhla <mokoena3586@gmail.com>
2025-11-13 02:48:29 +0200

    Corrected variable hours and seconds

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,8 +1,9 @@
 #Convert Hours to Seconds.
 
-hour = 2
+hours = 2
 seconds = 3600
 
-hours = 3600 * 2
+#There are 3600 seconds in an hour (since there are 60 minutes in an hour and 60 seconds in a minute, thus 60 x 60 = 3600).
+seconds = hours * 3600
 
-print(hour, "hour(s)", "is", hours, "seconds.")
+print(hours, "hour(s)", "is", seconds, "seconds.")

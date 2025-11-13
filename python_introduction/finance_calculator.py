5ec0e5bfad48c376b5514b53d4345a345e13585a
Nonhlanhla <mokoena3586@gmail.com>
2025-11-13 14:01:17 +0200

    Change the variable total_monthly_expenses to monthly_expenses

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,11 @@
 #Personal Finance Calculator. This program calculates the user’s monthly savings based on inputted monthly income and expenses.
 
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 simple_annual_interest = 0.05   #Simple annual interest rate is 5%     
 
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_savings = monthly_income - monthly_expenses
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 

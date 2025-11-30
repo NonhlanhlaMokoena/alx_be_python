22f77abd939ac88d01f5b077d441e11362c1fbfa
Nonhlanhla <mokoena3586@gmail.com>
2025-11-30 15:30:24 +0200

    Basic arithmetic operations function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+#A script that define a function that performs basic arithmetic operations. 
+
+def perform_operation(num1, num2, operation):
+    
+    if operation == "add":
+        return num1 + num2
+    
+    elif operation == "subtract":
+        return num1 - num2
+    
+    elif operation == "multiply":
+        return num1 * num2
+    
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Cannot divide by zero"
+        return num1 / num2
+    
